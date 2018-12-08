13851f30f32ef24651f5f1a834a48faab10531e4
andizhang <andybug@hotmail.com>
2018-12-08 15:37:32 -0500

    nits

--- IndexingMain.java
+++ IndexingMain.java
@@ -29,5 +29,21 @@ public class IndexingMain {
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.waitForCompletion(true);
+
+//        JobConf conf = new JobConf(IndexingMain.class);
+//        conf.setJobName("Indexing");
+//
+//        conf.setOutputKeyClass(Text.class);
+//        conf.setOutputValueClass(IntWritable.class);
+//
+//        conf.setMapperClass(IndexingMapper.class);
+////        conf.setCombinerClass(IndexingReducer.class);
+//        conf.setReducerClass(IndexingReducer.class);
+//        conf.setInputFormat(TextInputFormat.class);
+//        conf.setOutputFormat(TextOutputFormat.class);
+//        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+//        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+//        JobClient.runJob(conf);
+
     }
 }
--- IndexingReducer.java
+++ IndexingReducer.java
@@ -8,20 +8,20 @@ public class IndexingReducer extends Reducer<Text, Text, Text, Text>{
 	@Override
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 		/*Declare the Hash Map to store File name as key to compute and store number of times the filename is occurred for as value*/
-		HashMap m=new HashMap();
-		int count=0;
-		for(Text t:values){
-			String str=t.toString();
+		HashMap map = new HashMap();
+		int count = 0;
+		for(Text t: values){
+			String str = t.toString();
 			/*Check if file name is present in the HashMap ,if File name is not present then add the Filename to the HashMap and increment the counter by one , This condition will be satisfied on first occurrence of that word*/
-			if(m!=null &&m.get(str)!=null){
-				count=(int)m.get(str);
-				m.put(str, ++count);
+			if(map != null && map.get(str)!=null){
+				count = (int) map.get(str);
+				map.put(str, ++count);
 			}else{
 					/*Else part will execute if file name is already added then just increase the count for that file name which is stored as key in the hash map*/
-				m.put(str, 1);
+				map.put(str, 1);
 			}
 		}
 			/* Emit word and [file1→count of the word1 in file1 , file2→count of the word1 in file2 ………] as output*/
-		context.write(key, new Text(m.toString()));
+		context.write(key, new Text(map.toString()));
 	}
 }
