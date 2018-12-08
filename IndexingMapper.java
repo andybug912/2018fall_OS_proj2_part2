c8bfe28a6587338decba815a65d20131f9e5f5fb
andizhang <andybug@hotmail.com>
2018-12-08 16:40:55 -0500

    nits

--- IndexingMapper.java
+++ IndexingMapper.java
@@ -10,16 +10,14 @@ public class IndexingMapper extends Mapper<LongWritable,Text,Text,Text> {
 	public void map(LongWritable key, Text value, Context context)
 	throws IOException,InterruptedException
 	{
-		/*Get the name of the file using context.getInputSplit()method*/
 		String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+
 		String originLine = value.toString();
-		//Split the line in words
 		String line[] = originLine.toLowerCase().split(" ");
 		for(String wordWithPunctuation: line){
             String[] words = wordWithPunctuation.replaceAll("[!@#$%^&*()-=+,.?<>\'\"]", " ").trim().split(" ");
             for (String word: words) {
                 if (word.equals("") || word.charAt(0) < 'a' || word.charAt(0) > 'z') continue;
-                //for each word emit word as key and file name as value
                 context.write(new Text(word), new Text(fileName));
             }
 		}
